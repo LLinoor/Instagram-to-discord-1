5f0c9cd67c7891fdab8d880b3d923684378c4ee0
Lilian <pade.lilian@gmail.com>
2021-04-01 00:41:17 +0200

    first commit

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
-# Instagram to discord post images
+# Instagram-to-discord-1
 
-This script executes 2 actions:
+This script executes 3 actions:
 
 1. Monitors for new image posted in a instagram account.
 2. If found new image, a bot posts new instagram image in a discord channel.
 3. Repeat after set interval.
 
+<details><summary><b>Changelog (basic vs fork) :</b></summary>
+<p>
+
+- Add multi-user support
+- Improved embed
+- Bypass instagram's anti-bot security
+- Sends all the photos and videos that have been published during the interval (example: if the user sends 3 photos in the same minute, the bot will send the 3 photos)
+- Detection of a video
+- Ignore the 1st verification
+- Reduction of the verification interval
+- Removal of useless dependencies
+- Minor changes (optimization of functions, deletion of comments, ...)
+</p>
+</details>
+
 ## Requirements:
 
 - Python v3
-- Python module re, json, requests
+- requests (`pip install requests` or `pip3 install requests`)
 
 ## Usage:
 
@@ -17,7 +32,7 @@ Environment Variables:
 
 - Set IG_USERNAME to username account you want to monitor. Example: ladygaga
 - Set WEBHOOK_URL to Discord account webhook url. To know how, just Google: "how to create webhook discord".
-- Set TIME_INTERVAL to the time in seconds in between each check for a new post. Example: 1.5, 600 (default=600)
+- Set TIME_INTERVAL to the time in seconds in between each check for a new post. Example: 1.5, 600 (default=300)
 
 ## Collaborations:
 
--- instagram-discord.py
+++ instagram-discord.py
@@ -1,75 +1,48 @@
-#!/usr/bin/python
-
-# Copyright (c) 2020 Fernando
-# Url: https://github.com/fernandod1/
-# License: MIT
-
-# DESCRIPTION:
-# This script executes 2 actions:
-# 1.) Monitors for new image posted in a instagram account.
-# 2.) If found new image, a bot posts new instagram image in a discord channel.
-# 3.) Repeat after set interval.
-
-# REQUIREMENTS:
-# - Python v3
-# - Python module re, json, requests
-import re
 import json
-import sys
 import requests
-import urllib.request
 import os
 import time
+import datetime
 
-# USAGE:
-# Environment Variables
-# Set IG_USERNAME to username account you want to monitor. Example - ladygaga
-# Set WEBHOOK_URL to Discord account webhook url. To know how, just Google: "how to create webhook discord".
-# Set TIME_INTERVAL to the time in seconds in between each check for a new post. Example - 1.5, 600 (default=600)
+os.environ["firstCheck"] = "True"
+os.environ["WEBHOOK_URL"] = ""
+users = [""]
+os.environ["TIME_INTERVAL"] = "300"
 
-INSTAGRAM_USERNAME = os.environ.get('IG_USERNAME')
-
-# ----------------------- Do not modify under this line ----------------------- #
-
-
-def get_user_fullname(html):
-    return html.json()["graphql"]["user"]["full_name"]
-
-
-def get_total_photos(html):
-    return int(html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["count"])
-
+def get_profile_picture(html):
+    return html.json()["graphql"]["user"]["profile_pic_url_hd"]
 
 def get_last_publication_url(html):
     return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["shortcode"]
 
+def isVideo(html, selector):
+    mediaType = html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][selector]["node"]["is_video"]
+    if(mediaType == True):
+        mediaType = "video"
+    else:
+        mediaType = "pic"
+    return(mediaType)
 
-def get_last_photo_url(html):
-    return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["display_url"]
+def get_embed(html, selector):
+    embed = {}
+    embed["color"] = 15077485
+    embed["title"] = "@" + INSTAGRAM_USERNAME + " posted a new " + isVideo(html, selector)
+    embed["url"] = "https://www.instagram.com/p/" + html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][selector]["node"]["shortcode"] +"/"
+    try:
+        embed["description"] = html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][selector]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
+    except:
+        embed["description"] = ""
+    embed["image"] = {"url": html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][selector]["node"]["thumbnail_src"]}
+    embed["author"] = {"name":f"@{INSTAGRAM_USERNAME} (from Instagram)", "url":f"https://instagram.com/{INSTAGRAM_USERNAME}/", "icon_url":get_profile_picture(html)}
+    dt_object = datetime.datetime.fromtimestamp(html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][selector]["node"]["taken_at_timestamp"])
+    time = dt_object.strftime("%m/%d at %H:%M")
+    embed["footer"] = {"text": "• " + time + " (UTC Time)", "icon_url": "https://i.imgur.com/TqD7E3m.png"}
+    return embed
 
-
-def get_last_thumb_url(html):
-    return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"]
-
-
-def get_description_photo(html):
-    return html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
-
-
-def webhook(webhook_url, html):
-    # for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
-    # for all params, see https://discordapp.com/developers/docs/resources/channel#embed-object
+def webhook(webhook_url, html, selector):
     data = {}
     data["embeds"] = []
-    embed = {}
-    embed["color"] = 15467852
-    embed["title"] = "New pic of @"+INSTAGRAM_USERNAME+""
-    embed["url"] = "https://www.instagram.com/p/" + \
-        get_last_publication_url(html)+"/"
-    embed["description"] = get_description_photo(html)
-    # embed["image"] = {"url":get_last_thumb_url(html)} # unmark to post bigger image
-    embed["thumbnail"] = {"url": get_last_thumb_url(html)}
-    data["embeds"].append(embed)
+    data["embeds"].append(get_embed(html, selector))
     result = requests.post(webhook_url, data=json.dumps(
         data), headers={"Content-Type": "application/json"})
     try:
@@ -87,28 +60,61 @@ def get_instagram_html(INSTAGRAM_USERNAME):
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
     }
     html = requests.get("https://www.instagram.com/" +
+                        INSTAGRAM_USERNAME + "/channel/?__a=1", headers=headers)
+    try:
+        test_json = html.json()["graphql"]
+    except:
+        html = requests.get("https://www.instagram.com/" +
                         INSTAGRAM_USERNAME + "/feed/?__a=1", headers=headers)
+        try:
+            test_json = html.json()["graphql"]
+        except:
+            print("JSON Error")
     return html
 
 
 def main():
     try:
         html = get_instagram_html(INSTAGRAM_USERNAME)
-        if(os.environ.get("LAST_IMAGE_ID") == get_last_publication_url(html)):
+        if(os.environ["firstCheck"] == "True"):
+            print("First check, skipping the post")
+            os.environ[f"LAST_IMAGE_ID_{INSTAGRAM_USERNAME}"] = get_last_publication_url(html)
+        elif(os.environ.get(f"LAST_IMAGE_ID_{INSTAGRAM_USERNAME}") == get_last_publication_url(html)):
             print("Not new image to post in discord.")
         else:
-            os.environ["LAST_IMAGE_ID"] = get_last_publication_url(html)
             print("New image to post in discord.")
-            webhook(os.environ.get("WEBHOOK_URL"),
-                    get_instagram_html(INSTAGRAM_USERNAME))
+            i = 0
+            delete = True
+            for id_post in html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]:
+                id_post = id_post["node"]["shortcode"]
+                if(delete == True):
+                    if(id_post == os.environ[f"LAST_IMAGE_ID_{INSTAGRAM_USERNAME}"]):
+                        delete = False
+                    else:
+                        delete = True
+                else:
+                    pass
+            for post in html.json()["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]:
+                post = post["node"]["shortcode"]
+                if(post == os.environ[f"LAST_IMAGE_ID_{INSTAGRAM_USERNAME}"] or delete == True):
+                    os.environ[f"LAST_IMAGE_ID_{INSTAGRAM_USERNAME}"] = get_last_publication_url(html)
+                    return
+                else:
+                    webhook(os.environ.get("WEBHOOK_URL"), get_instagram_html(INSTAGRAM_USERNAME), i)
+                    i = i + 1
+
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-    if os.environ.get('IG_USERNAME') != None and os.environ.get('WEBHOOK_URL') != None:
+    if os.environ.get('WEBHOOK_URL') != None:
         while True:
-            main()
-            time.sleep(float(os.environ.get('TIME_INTERVAL') or 600))
+            for user in users:
+                INSTAGRAM_USERNAME = user
+                main()
+            if(os.environ["firstCheck"] == "True"):
+                os.environ["firstCheck"] = "False"
+            time.sleep(float(os.environ.get('TIME_INTERVAL') or 300))
     else:
         print('Please configure environment variables properly!')
